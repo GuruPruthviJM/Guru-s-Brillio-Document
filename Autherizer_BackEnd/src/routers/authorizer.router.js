356045a27961a6b66604871cb8b5add2c4d840b4
Guru Pruthvi J M <Guru.Pruthvi@brillio.com>
2025-02-05 21:32:52 +0530

    Authorizer #update 6

--- Autherizer_BackEnd/src/routers/authorizer.router.js
+++ Autherizer_BackEnd/src/routers/authorizer.router.js
@@ -1,22 +1,58 @@
 const express = require('express');
 const { expressx } = require('ca-webutils');
-const authorizerSchemaController = require('../controllers/authorizer.controller');
+const authorizerController = require('../controllers/authorizer.controller');
+const customerController = require('../controllers/customer.controller');
+const employeeController = require('../controllers/employee.controller');
+const adminController = require('../controllers/admin.controller');
+const managerController = require('../controllers/manager.controller');
 
 const createRouter = () => {
     const router = express.Router();
     let { routeHandler } = expressx;
-    let controller = authorizerSchemaController();
+    let authorizerControl = authorizerController();
+    let customerControl = customerController();
+    let managerControl = managerController();
+    let employeeControl = employeeController();
+    let adminControl = adminController();
 
     router
         .route('/')
-        .get(routeHandler(controller.getAllAuthorizers))
-        .post(routeHandler(controller.addAuthorizer));
+        .get(routeHandler(authorizerControl.getAllAuthorizers))
+        .post(routeHandler(authorizerControl.addAuthorizer));
 
     router
         .route('/:id')
-        .get(routeHandler(controller.getAuthorizerById))
-        .put(routeHandler(controller.updateAuthorizer))
-        .delete(routeHandler(controller.deleteAuthorizer));
+        .get(routeHandler(authorizerControl.getAuthorizerById))
+        .put(routeHandler(authorizerControl.updateAuthorizer))
+        .delete(routeHandler(authorizerControl.deleteAuthorizer));
+    
+    router
+        .route('/:authId/customers')
+        .get(routeHandler(customerControl.getAllCustomer))
+    
+    router
+        .route('/:authId/customers/:id')
+        .get(routeHandler(customerControl.getCustomerById))
+    
+    router
+        .route('/:authId/managers')
+        .get(routeHandler(managerControl.getAllManagers))
+    
+    router
+        .route('/:authId/managers/:id')
+        .get(routeHandler(managerControl.getManagerById))
+    
+    router
+        .route('/:authId/employees')
+        .get(routeHandler(employeeControl.getAllEmployees))
+    
+    router
+        .route('/:authId/customers/:id')
+        .get(routeHandler(employeeControl.getEmployeeById))
+    
+    //admin routes
+    
+    
 
     return router;
 }
