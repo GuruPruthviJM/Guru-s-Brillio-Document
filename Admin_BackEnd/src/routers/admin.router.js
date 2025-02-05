f8230ad1bbe75ae71b9752c75f67ff918127f663
Guru Pruthvi J M <Guru.Pruthvi@brillio.com>
2025-02-05 20:58:09 +0530

    admin update #5

--- Admin_BackEnd/src/routers/admin.router.js
+++ Admin_BackEnd/src/routers/admin.router.js
@@ -1,11 +1,16 @@
 const express = require('express');
 const { expressx } = require('ca-webutils');
 const adminController = require('../controllers/admin.controller');
+const employeeController = require('../controllers/employee.controller');
+const managerController = require('../controllers/manager.controller');
+
 
 const createRouter = () => {
     const router = express.Router();
     let { routeHandler } = expressx;
     let controller = adminController();
+    let employeeControl= employeeController();
+    let managerControl = managerController();
 
     router
         .route('/')
@@ -17,6 +22,26 @@ const createRouter = () => {
         .get(routeHandler(controller.getAdminById))
         .put(routeHandler(controller.updateAdmin))
         .delete(routeHandler(controller.deleteAdmin));
+    
+    router
+        .route('/:id/employee')
+        .get(routeHandler(employeeControl.getAllEmployees))
+        .post(routeHandler(employeeControl.addEmployee))
+    
+    router
+        .route('/:adminId/employee/:id')
+        .put(routeHandler(employeeControl.updateEmployee))
+        .delete(routeHandler(employeeControl.deleteEmployee));
+    
+    router
+        .route('/:id/manager')
+        .get(routeHandler(managerControl.getAllManagers))
+        .post(routeHandler(managerControl.addManager));
+
+    router
+        .route('/:adminId/manager/:id')
+        .put(routeHandler(managerControl.updateManager))
+        .delete(routeHandler(managerControl.deleteManager));
 
     return router;
 }
